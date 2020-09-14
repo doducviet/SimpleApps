5861870a972f41df3ecd4d75503bbd72e4f6d94c
Viet Do <25269296+doducviet@users.noreply.github.com>
2020-09-14 22:25:14 +0700

    Edit comment

--- SimpleApps/CSharpPlus/StringPlus.cs
+++ SimpleApps/CSharpPlus/StringPlus.cs
@@ -14,9 +14,8 @@ namespace CSharpPlus
         /// </summary>
         /// <param name="strInput"></param>
         /// <param name="strSeperator">A string that delimits the substrings in this string</param>
-        /// <param name="isRemoveEmptyEntries">(Optional, default is TRUE) omit empty array elements from
-        /// the array returned; or System.StringSplitOptions.None to include empty array
-        /// elements in the array returned.</param>
+        /// <param name="isRemoveEmptyEntries">(Optional, default is TRUE) TRUE : omit empty elements from the list returned; 
+        /// or FALSE to include empty elements in the list returned.</param>
         /// <returns></returns>
         public static List<string> Split(this string strInput, string strSeperator, bool isRemoveEmptyEntries = true)
         {

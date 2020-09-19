8293a7c0383acc09e5a7626b019e1535e229f7a5
Do Duc Viet <>
2020-09-19 23:37:51 +0700

    Add project : Generate_Class_Files_From_Database

---
+++ SimpleApps/Generate_Class_Files_From_Database/frmMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Generate_Class_Files_From_Database
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbConnectionString = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.clbTables = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(332, 6);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(92, 57);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbConnectionString
+            // 
+            this.tbConnectionString.Location = new System.Drawing.Point(12, 6);
+            this.tbConnectionString.Multiline = true;
+            this.tbConnectionString.Name = "tbConnectionString";
+            this.tbConnectionString.Size = new System.Drawing.Size(314, 57);
+            this.tbConnectionString.TabIndex = 2;
+            this.tbConnectionString.Text = "Enter connection string here";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(430, 6);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(92, 57);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // clbTables
+            // 
+            this.clbTables.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.clbTables.CheckOnClick = true;
+            this.clbTables.ColumnWidth = 250;
+            this.clbTables.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.clbTables.FormattingEnabled = true;
+            this.clbTables.Location = new System.Drawing.Point(0, 73);
+            this.clbTables.MultiColumn = true;
+            this.clbTables.Name = "clbTables";
+            this.clbTables.Size = new System.Drawing.Size(534, 377);
+            this.clbTables.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 450);
+            this.Controls.Add(this.clbTables);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.tbConnectionString);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "frmMain";
+            this.Text = "Generate class files from database";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbConnectionString;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.CheckedListBox clbTables;
+    }
+}
+

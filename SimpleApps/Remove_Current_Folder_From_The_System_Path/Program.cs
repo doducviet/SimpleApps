0e2deb14c3e6ad6506d30093b1932b4c86c66239
Viet Do <25269296+doducviet@users.noreply.github.com>
2021-02-15 15:56:15 +0700

    Add new project and update same target Debug, Release output folder for all projects

---
+++ SimpleApps/Remove_Current_Folder_From_The_System_Path/Program.cs
@@ -0,0 +1,57 @@
+﻿//    Copyright(C) 2020  Viet Do <https://github.com/doducviet>
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+using System;
+
+namespace Remove_Current_Folder_From_The_System_Path
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+
+            string name = "PATH";
+            EnvironmentVariableTarget scope = EnvironmentVariableTarget.Machine;
+            string oldValue = Environment.GetEnvironmentVariable(name, scope);
+
+            if (oldValue == null)
+            {
+                oldValue = string.Empty;
+            }
+
+            if (!oldValue.Contains(currentDirectory))
+            {
+                Console.WriteLine("Current folder is already removed from the system's path." + Environment.NewLine + "\t{0}",currentDirectory);
+            }
+            else
+            {
+                string newValue = oldValue.Replace(currentDirectory, "");
+                Environment.SetEnvironmentVariable(name, newValue, scope);
+
+                Console.WriteLine("Removed from the system's path:" + Environment.NewLine + "\t{0}", currentDirectory);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
